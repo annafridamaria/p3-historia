4ff0aeb4627a953c25b7c9c746ab7aef67106c5a
Frida Jonsson <annafridamariajonsson@gmail.com>
2020-02-27 18:52:15 +0100

    Started server.js
    Set up api, mongoose model, seed database

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ import cors from "cors";
 import mongoose from "mongoose";
 import episodeData from "./data/episode-data.json";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/P3Historia";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/historia";
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .catch(err => console.log(err));
 mongoose.Promise = Promise;
 
 // Defines the port the app will run on. Defaults to 8080, but can be
@@ -55,6 +60,29 @@ const Episode = mongoose.model("Episode", {
   }
 });
 
+// await new Episode({
+//   title: "Testisode",
+//   released: 20200227,
+//   century: 1600,
+//   description: "An episode",
+//   region: "Europe",
+//   country: "Sweden",
+//   sources: "Books",
+//   expert: "Frida",
+//   rating: 2
+// });
+// await new Episode({
+//   title: "Sarasode",
+//   released: 20190904,
+//   century: 1700,
+//   description: "An Sara episode",
+//   region: "Europe",
+//   country: "Sweden",
+//   sources: "Knowledge",
+//   expert: "sara",
+//   rating: 4
+// });
+
 // const importEpisodeData = () => {
 //   episodeData.forEach(episode => {
 //     new Episode(episode).save();
@@ -63,14 +91,31 @@ const Episode = mongoose.model("Episode", {
 
 // importEpisodeData();
 
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Episodes.deleteMany({});
+
+    data.forEach(episode => {
+      new Episode(episode).save();
+    });
+  };
+
+  seedDatabase();
+}
+
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello world");
-});
-
-app.get("/episodes", (req, res) => {
+  // res.send("Find episodes");
   res.json(episodeData);
 });
+
+// app.get("/episodes", (req, res) => {
+//   try {
+//     res.json(episodeData);
+//   } catch (err) {
+//     res.status(400);
+//   }
+// });
 
 // Start the server
 app.listen(port, () => {
