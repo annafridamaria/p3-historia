f76d5e76b1891a22ce46ec9546457f1ea7aacb28
Frida Jonsson <annafridamariajonsson@gmail.com>
2020-02-28 11:36:00 +0100

    Endpoints for specific episode, rating

--- server.js
+++ server.js
@@ -60,45 +60,77 @@ const Episode = mongoose.model('Episode', {
   }
 });
 
-// const importEpisodeData = () => {
-//   episodeData.forEach(episode => {
-//     new Episode(episode).save();
-//   });
-// };
+const importEpisodeData = () => {
+  episodeData.forEach(episode => {
+    new Episode(episode).save();
+  });
+};
 
 // importEpisodeData();
 
-  const seedDatabase = async () => {
-    await Episode.deleteMany({});
+  // const seedDatabase = async () => {
+  //   await Episode.deleteMany({});
 
-    episodeData.forEach(episode => {
-      new Episode(episode).save();
-    });
-  };
+  //   episodeData.forEach(episode => {
+  //     new Episode(episode).save();
+  //   });
+  // };
 
-  seedDatabase();
+  // seedDatabase();
 
 
 // Start defining your routes here
-app.get("/", (req, res) => {
+app.get("/episodes", (req, res) => {
   // res.send("Find episodes");
   res.json(episodeData);
 });
 
-app.get("/episode/:id", (req, res) => {
-  const id = req.params._id
-  Episode.findOne({'_id:': id})
-  .then((results) => {
-    res.json(results);
-  }).catch((err) => {
-    res.json({message: 'Cannot find this episode', err: err});
-  });
-  // res.send("Find episodes");
-  res.json(episodeData);
-});
 
+app.get('/episodes/:_id', async(req, res) => {
+  const {_id} = req.params;
+  try {
+    const episode = await Episode.findOne({'_id': _id})
+    // If not json is in the response, you have to invoke a send function
+    res.json(episode).status(201);
+  } catch (err) {
+    res.status(401).json({message: 'Episode not found', error:err})
+  }
+})
+
+app.get('/century/:century', async(req, res) => {
+  const {century} = req.params;
+  // try {
+    const centuryEpisodes = episodeData.filter((episode) => episode.century === century)
+    // If not json is in the response, you have to invoke a send function
+    res.json(centuryEpisodes).status(201);
+  // } catch (err) {
+  //   res.status(401).json({message: 'Episode not found', error:err})
+  // }
+})
+
+app.get('/rating/:rating', async(req, res) => {
+  const {rating} = req.params;
+  const ratedEpisodes = episodeData.filter((episode) => episode.rating === rating)
+  // If not json is in the response, you have to invoke a send function
+  res.json(ratedEpisodes).status(201);
+})
+
+
+// // Filter
+// app.get("/episode/:id", (req, res) => {
+//   const id = req.params._id
+//   Episode.findOne({'_id:': id})
+//   .then((results) => {
+//     res.json(results);
+//   }).catch((err) => {
+//     res.json({message: 'Cannot find this episode', err: err});
+//   });
+//   // res.send("Find episodes");
+//   res.json(episodeData);
+// });
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+// 5e58d0978d662707936f4763
