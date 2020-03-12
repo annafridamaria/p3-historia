4a444505cab7f53d6fd9a8eaf6bd7d7d9db3e33a
Frida Jonsson <annafridamariajonsson@gmail.com>
2020-03-12 13:28:41 +0100

    Moved into more components, route setup, register user setup

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
+import crypto from 'crypto';
+import bcrypt from 'bcrypt-nodejs';
 import episodeData from "./data/episode-data.json";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/P3historia";
@@ -23,7 +25,7 @@ const app = express();
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(bodyParser.json());
-
+// Episode using Review
 const Episode = mongoose.model('Episode', {
   title: {
     type: String,
@@ -40,6 +42,9 @@ const Episode = mongoose.model('Episode', {
   description: {
     type: String,
     required: true
+  },
+  image: {
+    type: String,
   },
   region: {
     type: String
@@ -58,10 +63,76 @@ const Episode = mongoose.model('Episode', {
     type: Number,
     default: 0
   },
+  reviews: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: 'Review'
+  },
   tags: {
     type: [String]
   }
 });
+// User using Review & Playlist
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    unique: true,
+    maxlength: 30,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken:  {
+    type: String,
+    default: ()=> crypto.randomBytes(128).toString('hex')
+  },
+  review: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Review'
+  },
+  playlist: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Playlist'
+  }
+})
+// Review using User & Episode
+const Review = mongoose.model('Review', {
+  episode: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Episode'
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  text: {
+    type: String,
+    maxlength: 1000,
+    required: true
+  }
+})
+// Playlist using User & Episode
+const Playlist = mongoose.model('Playlist', {
+  title: {
+    type: String,
+    maxlength: 100,
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  episodes: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: 'Episode'
+  }
+})
 
 const importEpisodeData = () => {
   episodeData.forEach(episode => {
@@ -69,12 +140,50 @@ const importEpisodeData = () => {
   });
 };
 
+const authenticateUser = async (req, res, next) => {
+  const user = await User.findOne({accessToken: req.header('Authorization')})
+  if (user) {
+    req.user = user;
+    next();
+  } else {
+    res.status(401).json({loggedOut: true})
+  }
+}
 // Start defining your routes here
 app.get("/episodes", (req, res) => {
   // res.send("Find episodes");
   res.json(episodeData);
 });
 
+app.post("/users", async (req, res) => {
+  try {
+    const {name, email, password} = req.body;
+    const user = new User({name, email, password: bcrypt.hashSync(password)});
+    await user.save()
+    res.status(201).json({id:user._id, accessToken: user.accessToken})
+    }catch(err){
+      res.status(400).json({message:'Could not create user', errors: err.errors})
+  }
+});
+app.get('/user', authenticateUser)
+app.get("/user", (req, res) => {
+  // res.send("Find episodes");
+  res.json({secret: "This is a user page"});
+});
+app.get("/login", (req, res) => {
+  // res.send("Find episodes");
+  res.json({secret: "This is a user page"});
+});
+
+app.get("/sessions", async(req, res) => {
+  const user = await User.findOne({email: req.body.email})
+  if (user && bcrypt.compareSync(req.body.password, user.password)){
+    res.json({userId: user._id, accessToken: user.accessToken})
+  } else {
+    res.json({notFound: true})
+  }
+
+});
 
 app.get('/episodes/:_id', async(req, res) => {
   const {_id} = req.params;
