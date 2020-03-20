463a7381d82267b94dba2933e689cc5354547263
Frida Jonsson <annafridamariajonsson@gmail.com>
2020-03-20 11:02:21 +0100

    Added netlify toml

---
+++ frontend/build/precache-manifest.aecfe7b55095336a238d27107c49eb8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b07d0640ef4108a08e56d983c4b64cb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ce19927907c13e7b2f1",
+    "url": "/static/css/main.85abb71e.chunk.css"
+  },
+  {
+    "revision": "716d4bf5c2fee72bad44",
+    "url": "/static/js/2.6c76b2f5.chunk.js"
+  },
+  {
+    "revision": "6d6c0d1371e570614ef23e12cbfed171",
+    "url": "/static/js/2.6c76b2f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ce19927907c13e7b2f1",
+    "url": "/static/js/main.d49e4087.chunk.js"
+  },
+  {
+    "revision": "f095f0bd1ebeac46ffb6",
+    "url": "/static/js/runtime-main.009da40f.js"
+  },
+  {
+    "revision": "be66d4128fad9740fb52cff67fae1ac0",
+    "url": "/static/media/marieantoinette.be66d412.png"
+  }
+]);
